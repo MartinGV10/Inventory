b3d39382607a7884d09b08caa1f4d76641547209
MartinGV10 <mganen10@gmail.com>
2026-01-02 00:14:17 -0500

    Added styling, minor fixes

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 const path = require('node:path')
+const playerRoutes = require('./routes/playerRoutes')
 
-app.set('view', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.set(express.urlencoded({ extended: true }))
+
+const assetsPath = path.join(__dirname, 'public')
+app.use(express.static(assetsPath))
+
+app.use('/', playerRoutes)
 
 const PORT = process.env.port || 3000
 

d3bcece75c85592603acf850170c0aad2e73748e
MartinGV10 <mganen10@gmail.com>
2026-01-02 13:07:32 -0500

    Added views for all tables

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -1,40 +1,108 @@
 const db = require('../db/queries')
 
+const links = [
+    { href: '/', text: 'Home'},
+    { href: '/players', text: 'Players'},
+    { href: '/clubs', text: 'Clubs'},
+    { href: '/contracts', text: 'Contracts'},
+    { href: '/listings', text: 'Listings'},
+    { href: '/transfers', text: 'Transfers'},
+    { href: '/create', text: 'Create New Player'},
+]
+
 async function getPlayers(req, res) {
     const players = await db.getAllPlayers()
     console.log(`Player -> ${players}`)
-    res.send(`Players -> ${players.map(player => player.full_name)}`)
+    // res.send(`Players -> ${players.map(player => player.full_name)}`)
+    res.render('viewItem', {
+        title: 'View Players',
+        links: links,
+        currentLink: req.path,
+        items: players,
+        attr: [
+            {key: 'full_name', label: 'Player Name'},
+            {key: 'nationality', label: 'Nationality'},
+            {key: 'height_cm', label: 'Height (CM)'},
+        ]
+    })
 }
 
 async function getClubs(req, res) {
     const clubs = await db.getAllClubs()
     console.log(`Club -> ${clubs}`)
-    res.send(`Club -> ${clubs.map(club => club.name)}`)
+    // res.send(`Club -> ${clubs.map(club => club.name)}`)
+    res.render('viewItem', {
+        title: 'View Clubs',
+        links: links,
+        currentLink: req.path,
+        items: clubs,
+        attr: [
+            {key: 'name', label: 'Club Name'},
+            {key: 'country', label: 'Country'},
+            {key: 'league', label: 'League'},
+        ]
+    })
 }
 
 async function getListings(req, res) {
     const listings = await db.getAllListings()
     console.log(`Listing -> ${listings}`)
-    res.send(`Listing -> ${listings.map(listing => listings.min_price)}`)
+    // res.send(`Listing -> ${listings.map(listing => listing.min_price)}`)
+    res.render('viewItem', {
+        title: 'View Listings',
+        links: links,
+        currentLink: req.path,
+        items: listings,
+        attr: [
+            {key: 'player_id', label: 'Listed Player'},
+            {key: 'selling_club_id', label: 'Selling Club'},
+            {key: 'status', label: 'Player Status'},
+            {key: 'asking_price', label: 'Asking Price'},
+            {key: 'min_price', label: 'Minimum Price'},
+            {key: 'notes', label: 'Notes'},
+        ]
+    })
 }
 
 async function getTransfers(req, res) {
     const trasnfers = await db.getAllTransfers()
     console.log(`Trasnfer -> ${trasnfers}`)
-    res.send(`Trasnfer -> ${trasnfers.map(trasnfer => trasnfer.fee)}`)
+    // res.send(`Trasnfer -> ${trasnfers.map(trasnfer => trasnfer.fee)}`)
+    res.render('viewItem', {
+        title: 'View Transfers',
+        links: links,
+        currentLink: req.path,
+        items: trasnfers,
+        attr: [
+            {key: 'player_id', label: 'Sold Player'},
+            {key: 'from_club_id', label: 'Selling Club'},
+            {key: 'to_club_id', label: 'Buying Club'},
+            {key: 'fee', label: 'Transfer Fee'},
+            {key: 'transfer_date', label: 'Date of Transfer'},
+            {key: 'notes', label: 'Notes'},
+        ]
+    })
 }
 
 async function getContracts(req, res) {
     const contracts = await db.getAllContracts()
     console.log(`Contract -> ${contracts}`)
-    res.send(`Contract -> ${contracts.map(contract => contract.shirt_number)}`)
+    // res.send(`Contract -> ${contracts.map(contract => contract.shirt_number)}`)
+    res.render('viewItem', {
+        title: 'View Contracts',
+        links: links,
+        currentLink: req.path,
+        items: contracts,
+        attr: [
+            {key: 'player_id', label: 'Player Under Contract'},
+            {key: 'club_id', label: 'Club'},
+            {key: 'shirt_number', label: 'Shirt Number'},
+            {key: 'start_date', label: 'Start Date'},
+            {key: 'end_date', label: 'End Date'},
+            {key: 'is_current', label: 'Are they currently playing here'},
+        ]
+    })
 }
-
-// function getSelection(req, res) {
-//     res.render('index', {
-//         title: 'Hi'
-//     })
-// }
 
 module.exports = {
     getPlayers,
@@ -42,5 +110,4 @@ module.exports = {
     getContracts,
     getTransfers,
     getListings,
-    // getSelection
 }
--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -9,13 +9,16 @@ const links = [
     { href: '/contracts', text: 'Contracts'},
     { href: '/listings', text: 'Listings'},
     { href: '/transfers', text: 'Transfers'},
+    { href: '/create', text: 'Create New Player'},
 ]
 
-playerRoutes.get('/', (req, res) => res.render('index', { links: links }))
+playerRoutes.get('/', (req, res) => res.render('index', { links: links, title: 'Homepage', currentLink: req.path }))
 
 
-// playerRoutes.get('/', playerController.getSelection)
 playerRoutes.get('/players', playerController.getPlayers)
 playerRoutes.get('/clubs', playerController.getClubs)
+playerRoutes.get('/contracts', playerController.getContracts)
+playerRoutes.get('/listings', playerController.getListings)
+playerRoutes.get('/transfers', playerController.getTransfers)
 
 module.exports = playerRoutes
